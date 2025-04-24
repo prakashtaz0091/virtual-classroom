54cec6c066763c98e4790a3771a13224351f6c44
prakashtaz0091 <code.ing2468@gmail.com>
2025-04-24 22:04:32 +0545

    auth

--- account/views.py
+++ account/views.py
@@ -1,3 +1,87 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import CustomUserCreationForm
+from .models import User, Profile
 
-# Create your views here.
+
+def register_view(request):
+    if request.method == 'POST':
+        data = request.POST
+        username = data.get('username')
+        password = data.get('password') 
+        confirm_password = data.get('confirm_password')
+        role = data.get('role')
+        firstname = data.get('firstname')
+        lastname = data.get('lastname')
+        
+        form = CustomUserCreationForm({
+            'username': username,
+            'password1': password,
+            'password2': confirm_password
+        })
+        
+        if form.is_valid():
+            user = User(
+                username=username,
+                role=role,
+                first_name=firstname,
+                last_name=lastname
+            )
+            user.set_password(password)
+            user.save()
+        else:
+            return render(request, 'account/register.html', {'form': form})
+        
+        
+        
+        #personal info
+        bio = data.get('bio')
+        profile_pic = request.FILES.get('profile_pic')
+        
+        try:
+            Profile.objects.create(
+                user=user,
+                bio=bio,
+                avatar=profile_pic
+            )
+        except Exception as e:
+            print(e)
+            context = {'error': 'An error occurred while creating the profile.'}
+            return render(request, 'account/register.html', context )   
+        
+        
+        return redirect('login_view')
+    
+    return render(request, 'account/register.html')
+
+
+
+def login_view(request):
+    
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        remember_me = request.POST.get('remember')
+        
+        # check if user exists with the provided credentials
+        user = authenticate(request, username=username, password=password)
+        
+        # if user exists, log them in
+        if user is not None:
+            login(request, user)
+            if remember_me:
+                request.session.set_expiry(1209600)  # 14 days in seconds
+            else:
+                request.session.set_expiry(0)  # Session expires at the end of the browser session
+            return redirect('main:home')
+        else:
+            return render(request, 'account/login.html', {'error': 'Invalid username or password'})
+        
+    
+   
+    return render(request, 'account/login.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login_view')
--- main/views.py
+++ main/views.py
@@ -1,22 +1,29 @@
 from django.shortcuts import render
 from .import models
+from django.contrib.auth.decorators import login_required
 
+
+@login_required
 def notes(request):
     return render(request, 'main/notes.html')
 
 
+@login_required
 def submission_detail(request):
     return render(request, 'main/submission_detail.html')
 
 
+@login_required
 def assignment_detail(request):
     return render(request, 'main/assignment_detail.html')
 
 
+@login_required
 def classroom(request):    
     return render(request, 'main/classroom.html')
 
 
+@login_required
 def home(request):
     
     all_classrooms = models.ClassRoom.objects.all()
